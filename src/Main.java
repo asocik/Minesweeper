865970690ad4b950e66a43af95fd63c50fbfa264
Adam Socik <socikadam@gmail.com>
2015-01-10 11:15:02 -0600

    Added README

--- src/Main.java
+++ src/Main.java
@@ -19,10 +19,37 @@ public class Main extends JFrame
 	public static void main(String[] args) throws IOException 
 	{
 		JFrame window = new MinesweeperGrid();
-		window.setTitle("Mineswepper");
+		window.setTitle("Minesweeper");
 		window.setVisible(true);
 		window.setSize(425, 500);
 		window.setLocationRelativeTo(null);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
